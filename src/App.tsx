5dfaa309cbcfc07758999947e38c932c4174dfaf
ribbinpo <ribbinpo@gmail.com>
2024-05-05 02:08:21 +0700

    feat: :triangular_flag_on_post: change to construction mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import MainLayout from "./layouts/main.layout";
-import HomePage from "./pages/home.page";
+// import MainLayout from "./layouts/main.layout";
+// import HomePage from "./pages/home.page";
 
 function App() {
   return (
-    <MainLayout>
-      <HomePage />
-    </MainLayout>
+    // <MainLayout>
+    //   <HomePage />
+    // </MainLayout>
+    <div className="bg-black text-center h-screen flex items-center justify-center">
+      <p className="text-6xl font-bold text-white animate-bounce">Coming soon...</p>
+    </div>
   );
 }
 
